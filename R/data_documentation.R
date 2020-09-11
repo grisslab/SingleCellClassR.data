b56ccad971783ac699ece614325f12bd7b66b15c
Vy Nguyen <thi-tuong-vy.nguyen@meduniwien.ac.at>
2020-09-11 11:52:14 +0000

    Initial commit
    add example data
    
    update datasets
    
    update plasma cells tag slot in objects
    
    reduce dataset size
    
    update features on datasets
    
    fix bioCviews terms

---
+++ R/data_documentation.R
@@ -0,0 +1,28 @@
+#' @name tirosh_mel80_example
+#' @title A Seurat Object Sample
+#' @description A Seurat object sample was made from the dataset GSE72056, samples corresponding to patient CY80.
+#' @docType data
+#' @usage tirosh_mel80_example
+#' @format a \code{\link{Seurat}} object
+#' @source WEIZMANN INSTITUTE OF SCIENCE
+#' @author Itay Tirosh, 2016-04-05
+#' @keywords datasets
+"tirosh_mel80_example"
+
+#' @name feldman_seurat
+#' @title A Seurat Object Sample
+#' @description Sade-Feldman dataset, analyzed and clustered using Seurat
+#' @docType data
+#' @usage feldman_seurat
+#' @author Vy Nguyen \email{thi-tuong-vy.nguyen@meduniwien.ac.at}
+#' @references Sade-Feldman et al.
+"feldman_seurat"
+
+#' @name jerby_seurat
+#' @title A Seurat Object Sample
+#' @description Sade-Feldman dataset, analyzed and clustered using Seurat
+#' @docType data
+#' @usage jerby_seurat
+#' @author Vy Nguyen \email{thi-tuong-vy.nguyen@meduniwien.ac.at}
+#' @references Jerby-Arnon et al.
+"jerby_seurat"
